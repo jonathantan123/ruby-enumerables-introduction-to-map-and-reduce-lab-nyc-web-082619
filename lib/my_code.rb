8f94f90bd94d7771aab44922211d6065ea242eb4
Jonathan Tan <jonathantan@brandeis.edu>
2019-08-22 18:20:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,54 @@
-# My Code here....
+def map_to_negativize(source_array) 
+  new_array=[]
+  
+  source_array.length.times do |counter| 
+    new_array.push(source_array[counter]* -1)
+  end 
+  
+  new_array 
+end 
+
+def map_to_no_change(source_array) 
+  new_array=[]
+  
+  source_array.length.times do|counter|
+  new_array << source_array[counter]
+end 
+new_array
+
+end 
+
+def map_to_double(source_array)
+  new_array= []
+  
+  source_array.length.times do |counter|
+    new_array << source_array[counter] * 2
+  end 
+  new_array
+end 
+
+def map_to_square(source_array)
+  new_array= []
+  source_array.length.times do |counter|
+    new_array << source_array[counter] ** 2 
+  end 
+  new_array
+end 
+
+def reduce_to_total(source_array, starting_point) 
+  source_array.length.times do |counter|
+    if starting_point
+      starting_point + source_array[counter]
+    else 
+      
+      
+end 
+  
+    
+  
+
+  
+
+
+    
+    
